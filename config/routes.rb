fc36d1a5ead46fe277c8103a08399bedc008bc7a
adonai-luque <akluqueprieto@gmail.com>
2021-11-03 16:17:28 -0500

    Added CRUD routes for developers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'developers/index'
-  get 'developers/show'
-  get 'developers/new'
-  get 'developers/create'
-  get 'developers/edit'
-  get 'developers/update'
-  get 'developers/update'
-  get 'developers/destroy'
   root "developers#index"
+  resources :developers
 end
